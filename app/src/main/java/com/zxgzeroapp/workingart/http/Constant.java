eeb55cd5ce7d8ace8b99b121a558f397245b89f4
a546530845 <546530845@qq.com>
2017-03-17 21:28:16 +0800

    Update Constant.java

--- app/src/main/java/com/zxgzeroapp/workingart/http/Constant.java
+++ app/src/main/java/com/zxgzeroapp/workingart/http/Constant.java
@@ -9,22 +9,10 @@ public class Constant {
     /**
      * 服务器名称
      */
-//    private static final String APP_SERVER_NAME = "http://192.168.1.118:7998/pos-provider/";
-//    /**
-//     * 测试服务器
-//     */
-//    private static final String APP_SERVER_NAME = "http://192.168.1.119:7998/pos-provider/";
-    /**
-     //    /**
-     //     * 域名外部服务器(预发布)
-     //     */
-//    private static final String APP_SERVER_NAME = "http://uat.dashufs.com/pos-provider/";
-    /**
-     * 生产服务器
-     */
-    public static final String APP_SERVER_NAME = "http://prod.dashufs.com/pos-provider/";
+//    private static final String APP_SERVER_NAME = "";
 
-    //    public static final String APP_SERVER_NAME = "http://xxxxxxxxxxxxxxxx/pos-provider/";
+
+
     public static final String APP_VERSION = "getCurrentVersion?";
     public static final String LOGIN = "checkLogin?";
     public static final String UPLOAD_IMAGE = "user/updateHeadUrl";
